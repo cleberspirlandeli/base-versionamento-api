a6eb107688958787378e421467f30a9ba0944c34
Cleber <contato.cleberrezende@gmail.com>
2020-10-09 01:12:28 -0300

    Ajustando dependency injection

--- src/DevIO.Api/Configurations/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configurations/DependencyInjectionConfig.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
 using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
@@ -14,13 +15,24 @@ namespace DevIO.Api.Configurations
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
+            #region DATA BASE
             services.AddScoped<MeuDbContext>();
+            #endregion
 
-            // Service
+            #region NOTIFICADORES
+            services.AddScoped<INotificador, Notificador>();
+            #endregion
+
+            #region SERVICES
             services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            #endregion
 
-            // Repository
+            #region REPOSITORIES
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            #endregion
 
             return services;
         }
