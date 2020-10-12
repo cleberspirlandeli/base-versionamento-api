2b32791b7b25e3f9bd9e62e0f4eed2199064434a
Cleber <contato.cleberrezende@gmail.com>
2020-10-12 13:55:58 -0300

    Finalizando JWT básico

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
 ﻿using DevIO.Api.DTO;
+using DevIO.Api.Extensions;
 using DevIO.Business.Intefaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DevIO.Api.Controllers
@@ -14,16 +20,18 @@ namespace DevIO.Api.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
-        //private readonly AppSettings _appSettings;
+        private readonly AppSettings _appSettings;
         //private readonly ILogger _logger;
 
         public AuthController(INotificador notificador,
                               SignInManager<IdentityUser> signInManager,
-                              UserManager<IdentityUser> userManager
+                              UserManager<IdentityUser> userManager,
+                              IOptions<AppSettings> appSettings
             ) : base(notificador)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("register")]
@@ -42,7 +50,7 @@ namespace DevIO.Api.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                return CustomResponse(registerUserDto);
+                return CustomResponse(GerarJwt());
                 //return CustomResponse(await GerarJwt(user.Email));
             }
             foreach (var error in result.Errors)
@@ -62,7 +70,7 @@ namespace DevIO.Api.Controllers
 
             if (result.Succeeded)
             {
-                return CustomResponse(loginUserDto);
+                return CustomResponse(GerarJwt());
                 //_logger.LogInformation("Usuario " + loginUserDto.Email + " logado com sucesso");
                 //return CustomResponse(await GerarJwt(loginUserDto.Email));
             }
@@ -74,6 +82,23 @@ namespace DevIO.Api.Controllers
 
             NotificarErro("Usuário ou Senha incorretos");
             return CustomResponse(loginUserDto);
+        }
+
+        private string GerarJwt()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            return encodedToken;
         }
     }
 }
