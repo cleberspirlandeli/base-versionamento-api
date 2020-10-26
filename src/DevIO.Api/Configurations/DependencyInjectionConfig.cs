71caed0641f955e6faa0528fde2505f8e30223d9
cleber rezende <contato.cleberrezende@gmail.com>
2020-10-25 21:20:53 -0300

    Continuando na implementacao do Swagger

--- src/DevIO.Api/Configurations/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configurations/DependencyInjectionConfig.cs
@@ -1,4 +1,5 @@
-﻿using DevIO.Api.Extensions;
+﻿using DevIO.Api.Configuration;
+using DevIO.Api.Extensions;
 using DevIO.Business.Intefaces;
 using DevIO.Business.Notificacoes;
 using DevIO.Business.Services;
@@ -6,6 +7,8 @@ using DevIO.Data.Context;
 using DevIO.Data.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace DevIO.Api.Configurations
 {
@@ -35,6 +38,7 @@ namespace DevIO.Api.Configurations
             #region OTHERS
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
             #endregion
 
             return services;
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -1,14 +1,14 @@
 using AutoMapper;
+using DevIO.Api.Configuration;
 using DevIO.Api.Configurations;
 using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System;
 
 namespace DevIO.Api
 {
@@ -35,27 +35,13 @@ namespace DevIO.Api
 
             services.WebApiConfig();
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1",
-                    new OpenApiInfo
-                    {
-                        Title = "API Com Versionamento - V1",
-                        Version = "v1",
-                        Description = "Exemplo de API REST criada com o ASP.NET Core 3.0 com versionamento.",
-                        Contact = new OpenApiContact
-                        {
-                            Name = "Cleber Rezende",
-                            Url = new Uri("https://github.com/cleberspirlandeli/versionamento-api")
-                        }
-                    });
-            });
+            services.AddSwaggerConfig();
 
             services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -72,10 +58,7 @@ namespace DevIO.Api
 
             app.UseMvcConfiguration();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Com Versionamento - V1");
-            });
+            app.UseSwaggerConfig(provider);
         }
     }
 }
