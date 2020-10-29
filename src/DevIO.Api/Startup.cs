c4f6b89556267a70685df78b22884cf5587f56db
cleber rezende <contato.cleberrezende@gmail.com>
2020-10-29 13:17:55 -0300

    incluindo imagens para readme e ajustes no swagger

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -46,13 +46,14 @@ namespace DevIO.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseCors("Development");
             }
             else
             {
                 app.UseHsts();
                 app.UseCors("Production");
             }
+
+            app.UseDeveloperExceptionPage();
 
             app.UseAuthentication();
 
